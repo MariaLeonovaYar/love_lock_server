4add4de9fecd1f9f0b55a9356a504d4344dcc24d
Maria Leonova <leonova-99@list.ru>
2020-01-10 15:12:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,34 +7,41 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
 CORS(app)
 
-cluster = MongoClient('mongodb+srv://atakhanova:Kate1245@cluster0-qp8rs.mongodb.net/test?retryWrites=true&w=majority')
+cluster = MongoClient('mongodb+srv://MariaLeo:provcolk13@cluster0-vfkmt.mongodb.net/test?retryWrites=true&w=majority')
 db = cluster['love_lock']
 values_collection = db['authorisation']
 lock_collection = db['lock']
 
-def get_lock_data_as_response_object(username):
+def get_data_as_response_object(username):
     response_object = {}
     results = lock_collection.find({'username': username})
     arr = []
     for el in results:
-        arr.append({'_id': str(el['_id']),'username': str(el['username']), 'person': str(el['person']), 'design': str(el['design']), 'message': str(el['message']), 'size': str(el['size'])})
+        arr.append({'_id': str(el['_id']), 'username': str(el['username']), 'size': str(el['size']), 'design': str(el['design']), 'person': str(el['person']), 'message': str(el['message'])})
     response_object['data'] = arr
     return response_object
 
-@app.route('/')
-@app.route('/api/get_lock_data', methods=['GET'])
-def get_lock_data():
+#проверка (есть ли такой пользователь)
+@app.route('/api/is_registered', methods=['GET'])
+def is_registered():
     if request.method == 'GET':
         username = request.args.get('username')
-        response_object = get_lock_data_as_response_object(username)
+        response_object = get_data_as_response_object(username)
         return jsonify(response_object)
 
-@app.route('/')
+#получение всех замков пользователя
+@app.route('/api/get_user_data', methods=['GET'])
+def get_user_data():
+    if request.method == 'GET':
+        username = request.args.get('username')
+        response_object = get_data_as_response_object(username)
+        return jsonify(response_object)
+
+#новый замок
 @app.route('/api/send_lock_data', methods=['POST'])
-def add_lock_data_into_db():
+def add_input_value_into_db():
     if request.method == 'POST':
         request_data = request.get_json()
         username = request_data.get('username')
@@ -42,59 +49,36 @@ def add_lock_data_into_db():
         design = request_data.get('design')
         size = request_data.get('size')
         message = request_data.get('message')
+        print(username)
         if (lock_collection.find().distinct('_id')):
             ID = max(lock_collection.find().distinct('_id'))+1
         else:
             ID = 0
         lock_collection.insert_one({"_id": ID, "username": username, "person" : person, "design": design, "size": size, "message": message})
         return jsonify({})
-    
-@app.route('/')
-@app.route('/login', methods=['POST'])
-def login():
-    users = db['authorisation']
-    response_object = {}
-    request_data = request.get_json()
-    username = request_data.get('username')
-    password = request_data.get('password')
-    login_user = users.find_one({'username' : str(username)})
-    if login_user:
-        if str(password) == login_user['password']:
-            response_object['message'] = str('true')
-            return response_object
-    response_object['message'] = str('false')
-    return response_object
 
-
-
-@app.route('/')
-@app.route('/register', methods=['POST'])
-def register():
-    users = db['authorisation']
-    request_data = request.get_json()
-    name = request_data.get('name')
-    surname = request_data.get('surname')
-    username = request_data.get('username')
-    password = request_data.get('password')
-    response_object = {}
-    existing_user = users.find_one({'username' : str(username)})
-    if (users.find().distinct('_id')):
-        ID = max(users.find().distinct('_id'))+1
-    else:
-        ID = 0
-    if existing_user is None:
-        users.insert_one({"_id": ID, 'name' : name,'surname' : surname,'username' : username, 'password' : password})
-        response_object['message'] = str('true')
-        return response_object
-    response_object['message'] = str('false')
-    return response_object
-
-@app.route('/')
+#удаление замка по айди
 @app.route('/api/delete_lock_id', methods=['POST'])
 def delete_lock():
     if request.method == 'POST':
         id_lock = request.get_json().get('id_lock')
         lock_collection.delete_one({'_id': id_lock})
+        return jsonify({})
+
+#регистрация
+@app.route('/api/send', methods=['POST'])
+def add_input_register_into_db():
+    if request.method == 'POST':
+        request_data = request.get_json()
+        name = request_data.get('name')
+        surname = request_data.get('surname')
+        username = request_data.get('username')
+        password = request_data.get('password')
+        if (values_collection.find().distinct('_id')):
+            ID = max(values_collection.find().distinct('_id'))+1
+        else:
+            ID = 0
+        values_collection.insert_one({"_id": ID, "name" : name, "surname": surname, "username": username, "password": password})
         return jsonify({})
 
 if __name__ == '__main__':
