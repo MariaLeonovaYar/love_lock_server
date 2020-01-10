3d64c767478970b92fdcdb3dbe8599eb0504bee3
Maria Leonova <leonova-99@list.ru>
2020-01-10 17:33:36 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,13 +74,13 @@ def register():
     response_object = {}
     existing_user = users.find_one({'username' : str(username)})
 
-    if (users.find().distinct('_id')):
-        ID = int(str(max(users.find().distinct('_id'))), 10)+1
-    else:
-        ID = 0
+    #if (users.find().distinct('_id')):
+     #   ID = int(str(max(users.find().distinct('_id'))), 10)+1
+   # else:
+    #    ID = 0
 
     if existing_user is None:
-        users.insert_one({"_id": ID, 'name' : name,'surname' : surname,'username' : username, 'password' : password})
+        users.insert_one({'name' : name,'surname' : surname,'username' : username, 'password' : password})
         response_object['message'] = str('true')
         return response_object
     response_object['message'] = str('false')
