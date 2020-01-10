8a9604d19d1d9a996f83d7d2c49026833841cb3d
Maria Leonova <leonova-99@list.ru>
2020-01-10 18:13:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -74,17 +74,16 @@ def register():
     password = request_data.get('password')
     response_object = {}
     existing_user = users.find_one({'username' : str(username)})
-    response_object = existing_user
     #if (users.find().distinct('_id')):
      #   ID = int(str(max(users.find().distinct('_id'))), 10)+1
    # else:
     #    ID = 0
 
-#     if existing_user is None:
-#         users.insert_one({'name' : name,'surname' : surname,'username' : username, 'password' : password})
-#         response_object['message'] = str('true')
-#         return jsonify(response_object)
-#     response_object['message'] = str('false')
+    if existing_user == null:
+         users.insert_one({'name' : name,'surname' : surname,'username' : username, 'password' : password})
+         response_object['message'] = str('true')
+         return jsonify(response_object)
+    response_object['message'] = str('false')
     return jsonify(response_object)
 
 @app.route('/api/delete_lock_id', methods=['POST'])
