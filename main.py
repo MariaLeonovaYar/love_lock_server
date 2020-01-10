2d7b060a34337977d71928c9553fa8e90c6fbadc
Maria Leonova <leonova-99@list.ru>
2020-01-10 15:43:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def register():
     response_object = {}
     existing_user = users.find_one({'username' : str(username)})
     if existing_user is None:
-        users.insert_one({'name' : name,'surname' : surname,'username' : username, 'password' : password})
+        users.insert_one({'name' : "11",'surname' : "11",'username' : "11", 'password' : "11"})
         response_object['message'] = str('true')
         return response_object
     response_object['message'] = str('false')
